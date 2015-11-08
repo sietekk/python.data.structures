15fa512b11d8fd875b2db944ec8a380419822188
Michael Conroy <michael@prometheusresearch.com>
2015-11-08 12:50:17 -0500

    Added Stack class

---
+++ src/basic/stack.py
@@ -0,0 +1,23 @@
+class Stack(object):
+    """ Stack implementation in python
+        Page 86, Listing 3.1
+        Implementation is O(1)
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
